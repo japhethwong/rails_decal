3aa3e26f2b4d28935a04a7250b38ea0ff19466e3
Japheth Wong <japhethjwong@gmail.com>
2015-03-13 23:35:19 +0000

    [Part 2] Implemented capturing Pokemon by updating button, adding Pokemon#capture method in controller, and updating routes.  Tested

--- proj1/app/controllers/pokemon_controller.rb
+++ proj1/app/controllers/pokemon_controller.rb
@@ -1,2 +1,8 @@
 class PokemonController < ApplicationController
+  def capture
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.trainer = current_trainer
+    @pokemon.save
+    redirect_to root_path
+  end
 end
